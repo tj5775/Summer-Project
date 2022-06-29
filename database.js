e2028f0bf20d18fd2a251b0a36ecafc4cd4f2e24
neoNite77 <abrahamdaungos@gmail.com>
2022-06-29 13:03:28 -0700

    Update database.js

--- database.js
+++ database.js
@@ -12,7 +12,7 @@ const client = new Client({
 client.connect(function(err) {
     if (err) throw err;
     console.log('Connected to the database!');
-    let query = "CREATE TABLE IF NOT EXISTS sensors (sensor_id serial PRIMARY KEY, name VARCHAR(50) NOT NULL, value VARCHAR(256), date DATE DEFAULT now())";
+    let query = "CREATE TABLE IF NOT EXISTS sensors (sensor_id serial PRIMARY KEY, name VARCHAR(50) NOT NULL, value Int, date DATE DEFAULT now())";
     client.query(query, (err, result) => {
         if (err) throw err;
         console.log(result)
