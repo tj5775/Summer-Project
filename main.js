0f5e5737739654dbee53531707cd90fcc444c03c
Amin <amin.hakem00@gmail.com>
2022-06-30 17:50:46 -0700

    added in-browser mqtt code

--- main.js
+++ main.js
@@ -5,6 +5,32 @@ var max = 0;
 var randomNumber;
 var randomNumbers = '';
 var sensor;
+//MQTT EXAMPLE
+//<----------------------------------------------
+var client = mqtt.connect("ws://13.57.29.105:8083/mqtt");
+const MQTT_TOPIC = "61c4b89a73bba8339e58a99f";
+
+console.log("Initializing MQTT client");
+
+client.on("connect", function () {
+  client.subscribe(MQTT_TOPIC, function (err) {
+    if (!err) {
+      console.log("Subscribed to topic");
+    }
+  });
+});
+
+client.on("message", function (topic, payload) {
+  console.log("Recieved message with topic " + topic);
+
+  var string = new TextDecoder("utf-8").decode(payload);
+  string = string.replace("\0", "");
+
+  let data = JSON.parse(string.trim());
+  console.log("Random Number -- " + data.randomNum);
+  console.log("\n");
+});
+//----------------------------------------------------->
 
 // Outputs a message based on the value selected from the sensorList
 function sensorSelection(){
