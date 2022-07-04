7128a749b6a4ee7fdf8dc7e571aee97ae3c79099
Amin <amin.hakem00@gmail.com>
2022-07-03 19:40:23 -0700

    added profile

---
+++ static/profile.js
@@ -0,0 +1,25 @@
+var host = "http://localhost:5500";
+
+if (localStorage.getItem("SavedToken") === null) {
+  window.location.href = "../login.html";
+}
+console.log(localStorage.getItem("SavedToken"));
+$.ajax({
+  type: "GET",
+  url: host + "/user/profile?token=" + localStorage.getItem("SavedToken"),
+  dataType: "JSON",
+  success: function (res) {
+    console.log(res);
+    $("#firstName").val(res.firsName);
+    $("#lastName").val(res.lastName);
+    $("#city").val(res.city);
+    $("#state").val(res.state);
+    $("#email").val(res.email);
+    $("#zip").val(res.zip);
+    $("#streetAddress").val(res.streetAddress);
+  },
+  error: function (xhr, textStatus, errorThrown) {
+    console.log("error : " + textStatus);
+    alert("failed");
+  },
+});
